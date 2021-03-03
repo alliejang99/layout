859c231dabe8d5043a6fb1bd3ff5a8072a39fac2
allie <69830149+alliejang99@users.noreply.github.com>
2021-03-03 18:59:36 +0900

    2021.03.03

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - 삭제:  npm uninstall node-sass
 - 5버전: npm install node-sass
 - 4버전: npm install node-sass@4.14.1
+
+### 규칙
+- routes: 페이지
+- component: 페이지 내의 내용
--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './routes/Header';
+// import Navigation from './routes/Navigation';
+import './scss/style.scss';
 
 function App() {
   return (
-    <div>
-      test
+    <div id="wrap">
+      <Header />
+      {/* <Navigation /> */}
     </div>
   )
 }
